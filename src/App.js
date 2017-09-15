490105bf060d69e4a7c5ea15ff996b2acb0307b2
teffcode <tefy.1@live.com>
2017-09-15 18:50:43 -0500

    ✏️ Title Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+
+import Title from './components/Title';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <div className="containerTitle">
+          <Title titleName="Emoji Todo List"/>
+        </div>
       </div>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Write the prop that will make our component reusable :3
+// Prop name -> titleName
+
+const Title = ({/* prop here */}) => {
+  return (
+    <div>
+       <h1></h1>
+    </div>
+  );
+}
+
+export default Title;
